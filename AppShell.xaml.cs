a0ded9a89324501bc9f23ce6717be322c61f3a77
naidosssik <naida.kasumova.04@mail.ru>
2026-09-04 00:47:30 +0300

    создание базового приложения

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OfflineMapApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OfflineMapApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
